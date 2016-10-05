36965ca4099a26d59768d8ecd38e8288d6abb14f
Erjan <erjanmx@gmail.com>
2016-10-05 19:11:51 +0300

    welcome page

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ def is_prime(n):
     return True
 
 
+@app.route("/")
+def welcome():
+    return "Welcome to simple rest server<br>" \
+           "usage<br>" \
+           "/prime/&lt;n&gt; - calculates the n-th prime number (<a target='_blank' href='/prime/55'>example</a>)<br>" \
+           "/factor/&lt;n&gt; - factorizes n (<a target='_blank' href='/factor/55'>example</a>)<br>" \
+           "/ping/&lt;host&gt;/&lt;n&gt; - pings given host n-times " \
+           "(<a target='_blank' href='/ping/google.com/5'>example</a>)<br>"
+
+
 @app.route("/prime/<n>")
 def prime(n):
     """
@@ -51,7 +61,7 @@ def factor(n):
             i = 2
         i += 1
     res.append(a)
-    return ','.join(str(i) for i in res)
+    return ', '.join(str(i) for i in res)
 
 
 @app.route("/ping/<host>/<n>")
